1b915a32546d0708bdf2ecdb29b100939aa189f7
patrick ouambo <patrickouambo@gmail.com>
2023-03-08 14:16:40 +0100

    performance example

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+
+const app = express();
+
+function delay(duration) {
+	const startTime = Date.now();
+	while (Date.now() - startTime < duration) {
+		// event loop is blocked
+	}
+}
+
+app.get('/', (req, res) => {
+	return res.send('Perfornmance example');
+});
+
+app.get('/timer', (req, res) => {
+	delay(9000);
+	return res.send('Ding ding ding');
+});
+
+app.listen(3000, () => {
+	console.log('Listening on port 3000');
+});
